815ea9e556be973c46812ddc42c0f1393498281f
Pavel Barto <pavelbarto@gmail.com>
2021-10-18 18:37:59 +0200

    doplnenie programu stvorec o vypocet obdlznika

--- stvorec.py
+++ stvorec.py
@@ -4,3 +4,10 @@ strana = float(input('Zadaj stranu štvorca v cemtimetroch: '))
 
 print('Obvod čtverce se stranou', strana, 'je', 4 * strana, 'cm')
 print('Obsah čtverce se stranou', strana, 'je', strana * strana, 'cm2')
+
+def obsah_obdelnika(a, b):
+    return a * b
+
+a = float(input('Zadaj stranu a obdĺžnika: '))
+b = float(input('Zadaj stranu b obdĺžnika: '))
+print('Obsah obdélníka se stranami ', a, ' cm a ', b, ' cm je', obsah_obdelnika(a, b), 'cm2')
