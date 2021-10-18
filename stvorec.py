00e5e19dcb22401dc25baca29dd81423dd5529bc
Pavel Barto <pavelbarto@gmail.com>
2021-10-18 17:56:15 +0200

    pridanie noveho cvicenia

---
+++ stvorec.py
@@ -0,0 +1,6 @@
+# tento program vypočíta obvod a obsah štvorca
+
+strana = 123
+
+print('Obvod čtverce se stranou', strana, 'je', 4 * strana, 'cm')
+print('Obsah čtverce se stranou', strana, 'je', strana * strana, 'cm2')
