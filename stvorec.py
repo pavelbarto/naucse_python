5094a9bf8a22a2726c5e94d0ffb04188f4fd5303
Pavel Barto <pavelbarto@gmail.com>
2021-10-18 18:11:11 +0200

    pridanie input do programu

--- stvorec.py
+++ stvorec.py
@@ -1,6 +1,6 @@
 # tento program vypočíta obvod a obsah štvorca
 
-strana = 123
+strana = float(input('Zadaj stranu štvorca v cemtimetroch: '))
 
 print('Obvod čtverce se stranou', strana, 'je', 4 * strana, 'cm')
 print('Obsah čtverce se stranou', strana, 'je', strana * strana, 'cm2')
