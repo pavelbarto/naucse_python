62d131fe25cef5268572c836348902f951e558f9
PavelBarto <pavelbarto@gmail.com>
2021-11-18 18:21:42 +0100

    doplnenie programu o vypocet kruhu

--- stvorec.py
+++ stvorec.py
@@ -1,9 +1,14 @@
 # tento program vypočíta obvod a obsah štvorca
 
 strana = float(input('Zadaj stranu štvorca v cemtimetroch: '))
+pi = 3.1415926
+
 
 print('Obvod čtverce se stranou', strana, 'je', 4 * strana, 'cm')
 print('Obsah čtverce se stranou', strana, 'je', strana * strana, 'cm2')
+print("Obvod kruhu s polomerom", strana, "je", 2 * pi * strana, "cm")
+print("Obsah kruhu s polomerom", strana, "je", pi * (strana * strana), "cm2")
+
 
 def obsah_obdelnika(a, b):
     return a * b
